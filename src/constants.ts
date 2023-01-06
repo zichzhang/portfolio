7b20bff47c70a29c9c7aeec23d72a7068e00ac5c
zichzhang <zi.c.zhang@mail.mcgill.ca>
2023-01-05 21:57:54 -0500

    added updated resume

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const RESUME = '';
+export const RESUME = 'https://drive.google.com/file/d/1eitMiPJ3qNQYvYm4pe0X7EMSncgCx2FU/view?usp=sharing';
 export const TWITTER_PROFILE = 'https://twitter.com/zich0x';
 export const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/zichaozhang';
 export const GITHUB_PROFILE = 'https://github.com/zichzhang/';
